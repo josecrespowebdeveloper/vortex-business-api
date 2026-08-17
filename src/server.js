6029dfcb53954ceeb79fae19f0951435135d06f5
Jose Crespo <crespovargasjosealberto@gmail.com>
2026-08-16 20:50:25 -0400

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Vortex Business API funcionando correctamente",
+    status: "online"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
